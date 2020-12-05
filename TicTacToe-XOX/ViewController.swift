0496ff613e6b403b7e9eba9ab546604021874c1b
Yeliz Kececi <yelizkececi>
2020-12-05 23:14:12 +0300

    Initial Commit

---
+++ TicTacToe-XOX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe-XOX
+//
+//  Created by Yeliz Keçeci on 5.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
